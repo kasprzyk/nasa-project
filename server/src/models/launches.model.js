51b4ebbe43f2d6816138447be6fd9acee07560c1
pkasprzycki <pkasprzycki@sii.pl>
2023-07-18 16:18:54 +0200

    POST /launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,14 +1,16 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
     destination: 'Kepler-442 b',
     flightNumber: 100,
-    customer: ['NASA', 'SpaceX'],
     upcoming: true,
     success: true,
+    customer: ['NASA', 'SpaceX'],
 };
 
 launches.set(launch.flightNumber, launch);
@@ -17,6 +19,20 @@ function getAllLaunches() {
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            upcoming: true,
+            success: true,
+            flightNumber: latestFlightNumber,
+            customer: ['NASA', 'SpaceX'],
+        }),
+    );
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 };

33e36a8c762fc9ba7b66ad1e8f955ada147054fd
pkasprzycki <pkasprzycki@sii.pl>
2023-10-29 21:44:25 +0100

    mongodb connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,26 @@
 const http = require('http');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL;
 const { loadPlanetsData } = require('./models/planets.model');
 const app = require('./app');
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error(error);
+});
+
 async function startServer() {
+    mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`);

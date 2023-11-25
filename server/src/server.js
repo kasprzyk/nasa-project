1a653dccc73993e08c195b3030640a7285bf6e0c
pkasprzycki <pkasprzycki@sii.pl>
2023-11-25 13:59:16 +0100

    Updating tests for mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,19 @@
 const http = require('http');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-dotenv.config();
-const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGO_URL;
-const { loadPlanetsData } = require('./models/planets.model');
+
+require('dotenv').config();
+
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
+
+const PORT = process.env.PORT || 8000;
+
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready');
-});
-
-mongoose.connection.on('error', (error) => {
-    console.error(error);
-});
-
 async function startServer() {
-    mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    await mongoConnect();
     await loadPlanetsData();
+
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`);
     });

4768c45dd9a03f8c9f540e98cf51769a02dcb8bb
pkasprzycki <pkasprzycki@sii.pl>
2023-12-11 12:51:28 +0100

    Updating tests

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,6 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
-
 const api = require('./routes/api');
 const app = express();
 
@@ -20,9 +17,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/v1', api);
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 

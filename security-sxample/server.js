beecac12a890886a0d9575d8938cac93ed195cc3
pkasprzycki <pkasprzycki@sii.pl>
2024-01-08 12:01:45 +0100

    Helmet.js

--- security-sxample/server.js
+++ security-sxample/server.js
@@ -1,10 +1,11 @@
-const https = require('https');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
     return res.send('Your personal secret value is 42!');
@@ -14,11 +15,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-https
-    .createServer({
-        key: '',
-        cert: '',
-    })
-    .listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`);
-    });
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});

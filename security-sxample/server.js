16189d8d8a0120b27d4a2509815ac3648088a3c6
pkasprzycki <piotkas1@gmail.com>
2024-02-10 17:16:50 +0100

    test commit

--- security-sxample/server.js
+++ security-sxample/server.js
@@ -86,6 +86,8 @@ app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Your personal secret value is 42!');
 });
 
+//test
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });

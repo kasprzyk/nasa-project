33dd79fdd0b1ca7c57a6142c316d27bdae8ee331
pkasprzycki <pkasprzycki@sii.pl>
2024-02-10 17:04:35 +0100

    cookie-session

--- security-sxample/server.js
+++ security-sxample/server.js
@@ -18,6 +18,8 @@ const PORT = 3000;
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -35,14 +37,14 @@ passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 app.use(helmet());
-app.use(session);
-// app.use(
-//     cookieSession({
-//         name: 'session',
-//         maxAge: 24 * 60 * 60 * 1000,
-//         keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
-//     }),
-// );
+//app.use(session);
+app.use(
+    cookieSession({
+        name: 'session',
+        maxAge: 24 * 60 * 60 * 1000,
+        keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+    }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 

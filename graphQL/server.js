ced8bdcfde7fd7a4fb65636869a0d1a412b9b25e
pkasprzycki <piotkas1@gmail.com>
2024-02-14 18:50:21 +0100

    graphql in node

--- graphQL/server.js
+++ graphQL/server.js
@@ -1,9 +1,30 @@
-const path = require("path");
-const cors = require("cors");
-const express = require("express");
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
 
+const schema = buildSchema(`
+    type Query {
+        description: String
+        price: Float
+    }
+`);
+
+const root = {
+    description: 'test',
+    price: 12.23,
+};
 const app = express();
 
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+    }),
+);
+
 app.listen(3000, () => {
-  console.log("Running GraphQL server...");
+    console.log('Running GraphQL server...');
 });
